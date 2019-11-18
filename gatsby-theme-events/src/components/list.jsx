282d5ce87ba641bb548976898ec00dc254da4c30
Lennart <lekoarts@gmail.com>
2019-11-18 19:22:28 +0100

    step 06: Sort Events by upcoming and past

--- gatsby-theme-events/src/components/list.jsx
+++ gatsby-theme-events/src/components/list.jsx
@@ -2,13 +2,24 @@
 import React from 'react'
 import { Styled, jsx } from 'theme-ui'
 import ListItem from './list-item'
+import sortEvents from '../utils/sort-events'
 
 const List = ({ events }) => {
+  const { upcoming, past } = sortEvents(events)
+
   return (
     <section sx={{ variant: `list.default` }}>
       <Styled.h2>Upcoming Events</Styled.h2>
       <ul>
-        {events.map(event => (
+        {upcoming.map(event => (
+          <li key={event.name}>
+            <ListItem event={event} />
+          </li>
+        ))}
+      </ul>
+      <Styled.h2>Past Events</Styled.h2>
+      <ul>
+        {past.map(event => (
           <li key={event.name}>
             <ListItem event={event} />
           </li>
