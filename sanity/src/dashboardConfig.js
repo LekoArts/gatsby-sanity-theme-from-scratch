bad1be477016bd3cca63ceb585d4594f4589fd60
Lennart <lekoarts@gmail.com>
2019-11-19 13:13:20 +0100

    Bonus: Add Gatsby Preview integration

---
+++ sanity/src/dashboardConfig.js
@@ -0,0 +1,14 @@
+export default {
+  widgets: [
+    {
+      name: "gatsby",
+      options: {
+        instances: [
+          {
+            instanceUrl: "https://gatsby-sanity-theme-from-scratch-5575687666.gtsb.io",
+          },
+        ],
+      },
+    },
+  ],
+}
